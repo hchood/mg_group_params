a464a6ded92bdc80acce7b9840084f331df9a33a
Helen Hood <helen.c.hood@gmail.com>
2014-08-20 09:32:00 -0400

    Add comments

--- server.rb
+++ server.rb
@@ -2,3 +2,9 @@ require 'sinatra'
 require 'shotgun'
 require 'csv'
 require 'pry'
+
+# Create pages for:
+# - Page that lists all the artists & links to artist's page
+# - Page for each artist.  Has name, genre, and then name of each of their songs
+# - Page that lists all songs
+# - Page for each song, showing name of song & artist name
